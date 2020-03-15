1861dfb2266fac4d2f4e8871b1fa87bce7041fc0
lucaswatanuki <lucaswatanuki@gmail.com>
2020-03-15 20:46:23 -0300

    Estrutura inicial usando cupertino component

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      home: BytebankHomePage(),
+    );
+  }
+}
+
+class BytebankHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.time),
+            title: Text('Histórico'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.add_circled),
+            title: Text('Transferir'),
+          ),
+        ],
+      ),
+      tabBuilder: (BuildContext context, int index) {
+        return CupertinoTabView(
+          builder: (BuildContext context) {
+            return CupertinoPageScaffold(
+              navigationBar: CupertinoNavigationBar(
+                middle: Text('Page 1 of tab $index'),
+              ),
+              child: Center(
+                child: CupertinoButton(
+                  child: const Text('Next page'),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      CupertinoPageRoute<void>(
+                        builder: (BuildContext context) {
+                          return CupertinoPageScaffold(
+                            navigationBar: CupertinoNavigationBar(
+                              middle: Text('Page 2 of tab $index'),
+                            ),
+                            child: Center(
+                              child: CupertinoButton(
+                                child: const Text('Back'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
