946952e191bd1d1f0c375b2286a7b07505299717
lucaswatanuki <lucaswatanuki@gmail.com>
2020-03-16 23:24:50 -0300

    Extrações

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,41 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: ListaTransferencia(),
-        appBar: AppBar(
-          title: Text("Transferências"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
+        body: FormularioTransferencia(),
       ),
-    ));
+    );
+  }
+}
+
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListaTransferencia(),
+      appBar: AppBar(
+        title: Text("Transferências"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
 
 class ListaTransferencia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        ItemTransferencia(Transferencia(100, 1)),
+        ItemTransferencia(Transferencia(500, 1)),
         ItemTransferencia(Transferencia(200, 2)),
         ItemTransferencia(Transferencia(300, 3)),
       ],
