8a5189bd7aa0b2857287ae8064bfa9ca0402b223
lucaswatanuki <lucaswatanuki@gmail.com>
2020-04-12 13:52:15 -0300

    Refatorações na estrutura do código + dark theme

---
+++ lib/screens/transferencias/formulario.dart
@@ -0,0 +1,59 @@
+import 'package:bytebank/components/editor.dart';
+import 'package:bytebank/models/transferencias.dart';
+import 'package:flutter/material.dart';
+
+const appBarTitle = 'Criando Transferência';
+const dicaText = '0000';
+const numeroConta = 'Número da conta';
+const valor = 'Valor';
+
+class FormularioTransferencia extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return FormularioTransferenciaState();
+  }
+}
+
+class FormularioTransferenciaState extends State<FormularioTransferencia> {
+  final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(appBarTitle),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Editor(
+                controlador: _controladorCampoNumeroConta,
+                dica: dicaText,
+                rotulo: numeroConta,
+              ),
+              Editor(
+                dica: dicaText,
+                controlador: _controladorCampoValor,
+                rotulo: valor,
+                icone: Icons.monetization_on,
+              ),
+              RaisedButton(
+                child: Text('Confirmar'),
+                onPressed: () => _criaTransferencia(context),
+              )
+            ],
+          ),
+        ));
+  }
+
+  void _criaTransferencia(BuildContext context) {
+    final int numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+    final double valor = double.tryParse(_controladorCampoValor.text);
+    if (numeroConta != null && valor != null) {
+      final transferenciaCriada = Transferencia(valor, numeroConta);
+      Navigator.pop(context, transferenciaCriada);
+    }
+  }
+}
---
+++ lib/screens/transferencias/lista.dart
@@ -0,0 +1,63 @@
+import 'package:bytebank/models/transferencias.dart';
+import 'package:bytebank/screens/transferencias/formulario.dart';
+import 'package:flutter/material.dart';
+
+class ListaTransferencias extends StatefulWidget {
+  final List<Transferencia> _transferencias = new List();
+
+  @override
+  State<StatefulWidget> createState() {
+    return ListaTransferenciaState();
+  }
+}
+
+class ListaTransferenciaState extends State<ListaTransferencias> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+      body: ListView.builder(
+        itemCount: widget._transferencias.length,
+        itemBuilder: (context, index) {
+          final transferencia = widget._transferencias[index];
+          return ItemTransferencia(transferencia);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return FormularioTransferencia();
+          })).then((transferenciaRecebida) =>
+              _atualizaTransferencia(transferenciaRecebida));
+        },
+      ),
+    );
+  }
+
+  void _atualizaTransferencia(Transferencia transferenciaRecebida) {
+    if (transferenciaRecebida != null) {
+      setState(() {
+        widget._transferencias.add(transferenciaRecebida);
+      });
+    }
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      leading: Icon(Icons.monetization_on),
+      title: Text("Valor: " + _transferencia.valor.toString()),
+      subtitle: Text("C/C: " + _transferencia.numeroConta.toString()),
+    ));
+  }
+}
